2e5a2d9cb2d5e556cba03b307649f042613248bf
GyuMin <126860086+kornet79@users.noreply.github.com>
2024-05-17 10:44:10 +0900

    Create AnimalSoundMain3.java

---
+++ 추상 클래스 & 추상 메서드/ex2/AnimalSoundMain3.java
@@ -0,0 +1,28 @@
+package poly.ex2;
+
+public class AnimalSoundMain3 {
+
+    public static void main(String[] args) {
+        Animal[] animals = {new Dog(), new Cat(), new Caw()};
+
+        for (Animal animal : animals) {
+            soundAnimal(animal);
+        }
+    }
+
+    public static void soundAnimal(Animal animal) {
+        // 부모는 모두 똑같기 떄문에 같은 값을 파라미터로 받을수 있다.
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+
+
+}
+
+/*
+2가지 문제점
+1. Animal 클래스를 생성할 수 있는 문제
+2. Animal 클래스를 상속 받는 곳에서 sound() 메서드 오버라이딩 하지 않을 가능성
+
+ */
