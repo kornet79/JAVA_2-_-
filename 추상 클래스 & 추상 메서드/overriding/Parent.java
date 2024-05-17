33264fa2f09f35736edb8408b4e171986a6d8f62
GyuMin <126860086+kornet79@users.noreply.github.com>
2024-05-17 10:42:00 +0900

    Create Parent.java

---
+++ 추상 클래스 & 추상 메서드/overriding/Parent.java
@@ -0,0 +1,11 @@
+package poly.overriding;
+
+public class Parent {
+    public String value = "parent";
+
+    public void method() {
+        System.out.println("Parent.mathod");
+    }
+
+    // 필드값과 메소드 하나를 만들었다.
+}
