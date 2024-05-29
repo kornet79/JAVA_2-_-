bcfc0b9280a29bc8b894bf5c4c6c26a1fb47f151
GyuMin <126860086+kornet79@users.noreply.github.com>
2024-05-29 09:19:58 +0900

    Create K3Car.java

---
+++ 인터페이스/car0/K3Car.java
@@ -0,0 +1,21 @@
+package poly.car0.car0;
+
+public class K3Car {
+
+
+    public void startEngine() {
+        System.out.println("K3Car.startEngine");
+    }
+
+    public void pressAccelerator() {
+        System.out.println("K3Car.pressAccelerator");
+    }
+
+    public void offEngine() {
+        System.out.println("K3Car.offEngine");
+    }
+
+
+
+
+}
